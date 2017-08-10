5613120b204f1babca415d0f5a38c9bb667358cc
Nick Foden <nickfoden@gmail.com>
2017-08-10 11:26:44 -0400

    Children Match

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-const Home = () => <h1> Home </h1>;
+const Home = (props) => {
+  console.log(props)
+  return <h1>Home</h1>
+}
 
 const App = () => (
   <Router>
-    <Route path="/" component={Home} />
-  </Router>  
-);
+  	<div>
+    	<Route exact path="/" component={Home} />
+    	{/*<Route path="/about" render={() => <h1> About</h1>} />*/}
+    	<Route
+      path="/about"
+      children={({match}) => match && <h1> About</h1>} />
+    </div>
+  </Router>
+)
 
-export default App
+export default App;
